c7f2bfba1440dc6eed0df5cf5824a167350a3678
nuneskn <60264086+nuneskn@users.noreply.github.com>
2020-08-17 15:40:09 -0400

    Added input options

--- Gem.java
+++ Gem.java
@@ -1,10 +1,19 @@
 public class Gem {
-
-
-
-
-
-
-
-
+  
+  int gem;
+    
+  
+  
+  
+  
+  
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter input type : 1 - manual entry, 2 - name of file");
+    int input = sc.nextInt();
+    if(input == 1){
+      System.out.println("Enter the number of gems");
+      gem = sc.nextInt();    
+    }
+  }
 }
