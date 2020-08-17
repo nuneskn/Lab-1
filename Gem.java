d8d8104760f1e3298b98b6548ae88cb92398283a
Nicole Nunes <nuneskn@appstate.edu>
2020-08-17 18:11:58 -0400

    Made it compile woo

--- Gem.java
+++ Gem.java
@@ -1,37 +1,49 @@
 import java.util.Scanner;
 import java.io.File;  
 import java.util.Random;
-
-
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Gem {
-  
-  int gem = 0;
-  int[gem] gemList;
-  Random random = new Random();
-  
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter input type : 1 - manual entry, 2 - name of file");
-    int input = sc.nextInt();
-    if(input == 1){
-      System.out.println("Enter the number of gems");
-      gem = sc.nextInt();    
-      for(int i = 0; i < gem; i++){
-          gem[i] = random.nextInt(50000);
-      }
+   
+    ArrayList pile1 = new ArrayList<Integer>();
+    ArrayList pile2 = new ArrayList<Integer>();
+    
+    public static int findSum(int num, int[] list){
+    
+        return 0; 
     }
-    else if(input == 2){
-      System.out.println("Enter name of the file");
-      String filename = sc.nextLine();
-      File myFile = new File(filename);
-      Scanner reader = new Scanner(myFile);
-      gem = reader.nextInt();
-      while(reader.hasNext()){
-        for(int i = 0; i < gem; i++){
-          gem[i] = reader.nextInt();
+     
+    public static void main(String[] args){
+        int gem;
+        int[] gemList;
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input type : 1 - manual entry, 2 - file based");
+        int input = sc.nextInt();
+        if(input == 1){
+            System.out.println("Enter the number of gems");
+            gem = sc.nextInt();
+            gemList = new int[gem];
+            for(int i = 0; i < gem; i++){
+                gemList[i] = random.nextInt(50000);
+            }
+            findSum(gem,gemList);
         }
-      }
-    }
-  }
+        if(input == 2){
+            System.out.println("Enter name of file");
+            String filename = sc.nextLine();
+            try{
+                File myFile = new File(filename);
+                Scanner reader = new Scanner(myFile);
+                gem = reader.nextInt();
+                gemList = new int[gem];
+                for(int i = 0; i < gem;i++){
+                    gemList[i] = reader.nextInt();
+                } 
+            } catch (FileNotFoundException e){
+                System.out.println("Couldn't find file");
+            }
+        }
+    }    
 }
