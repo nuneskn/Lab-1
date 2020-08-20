1735addfa66d600a522b5835dac611801650508b
Nicole Nunes <nuneskn@appstate.edu>
2020-08-20 13:57:59 -0400

    Added functions to divide the original list of randoms in half

--- Gem.java
+++ Gem.java
@@ -10,17 +10,46 @@ public class Gem {
     int[] pile1;
     int[] pile2;
 
-    public int average(int[] list){
+    public int halfAverage(int[] list){
         
         int average = 0;
         for(int i = 0; i < list.length; i++){
             average += list[i];
         }
-        return average;
+        return average / list.length;
     }
 
-    public void sum(int gem, int[] list){
-       
+    public int[] dividefirsthalf(int gem, int[] list){  
+       pile1 = new int[gem];
+       //pile2 = new int[gem];
+       for(int i = 0; i < list.length / 2; i++){
+                if(list[i] != 0){
+                    pile1[i] = list[i];
+                }
+                else{
+                    continue;
+                }      
+       }
+       /*for(int z = list.length / 2; z < list.length; z++){
+            for(int m = 0; m < gem; m++){
+                pile2[m] = list[z];
+            }
+       }*/
+        return pile1;
+    }
+
+    public int[] divideSecondHalf(int gem, int[] list){
+        pile2 = new int[gem / 2];
+        for(int i = 0; i < list.length; i++){
+            pile2[i] 
+        }
+        return pile2;
+    }
+
+    public void toString(int[] list){
+        for(int i = 0; i < list.length;i++){
+            System.out.print(" " + list[i] + " ");
+        }
     }
        
         
@@ -40,7 +69,9 @@ public class Gem {
             for(int i = 0; i < gem; i++){
                 gemList[i] =random.nextInt(50000);
             }
-            System.out.println(sort.average(gemList));
+            System.out.println(sort.halfAverage(gemList));
+            sort.toString(sort.dividefirsthalf(gem,gemList));
+            sort.toString(sort.divideSecondHalf(gem,gemList)); 
             //findSum(gem,gemList);
         }
         if(input == 2){
@@ -54,12 +85,10 @@ public class Gem {
                 for(int i = 0; i < gem;i++){
                     gemList[i] = reader.nextInt();
                 }
-                sort.average(gemList); 
+                sort.halfAverage(gemList); 
             } catch (FileNotFoundException e){
                 System.out.println("Couldn't find file");
             }
         }
-        //Gem sort = new Gem();
-        //sort.average(gemList);
     }    
 }
